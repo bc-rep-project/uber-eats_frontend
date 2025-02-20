9824838f2c67f2be5d21f0643756068118738eac
bc-rep-project <johannderbiel@gmail.com>
2025-02-20 18:12:44 +0200

    Add store and product image constants for grocery page
    - Create constants.ts with predefined image URLs for stores and products
    - Include images for stores like Lucky, Cardenas, Smart Final, CVS, and Chevron
    - Add product images for pancake mix and maple syrup
    - Centralize image references for consistent and easy management

---
+++ src/pages/grocery/constants.ts
@@ -0,0 +1,13 @@
+export const STORE_IMAGES = {
+  LUCKY: 'https://tb-static.uber.com/prod/image-proc/processed_images/2e7f4b23c6d9d8f9c05d8573ab50a8b1/3ac2b39ad528f8c8c5dc77c59abb683d.jpeg',
+  CARDENAS: 'https://tb-static.uber.com/prod/image-proc/processed_images/8c3c7ba0d36e8b95c2c2c3f9cc298994/3ac2b39ad528f8c8c5dc77c59abb683d.jpeg',
+  SMART_FINAL: 'https://tb-static.uber.com/prod/image-proc/processed_images/1d66fb8f6f9b3ea2d9f5b0054461bb8d/3ac2b39ad528f8c8c5dc77c59abb683d.jpeg',
+  CVS: 'https://tb-static.uber.com/prod/image-proc/processed_images/7c8de9248dd53316957048a1172e2d91/3ac2b39ad528f8c8c5dc77c59abb683d.jpeg',
+  CHEVRON: 'https://tb-static.uber.com/prod/image-proc/processed_images/9b05150e6b5b6c7ed0d74ccfb4c838b2/3ac2b39ad528f8c8c5dc77c59abb683d.jpeg',
+};
+
+export const PRODUCT_IMAGES = {
+  PANCAKE_MIX: 'https://tb-static.uber.com/prod/image-proc/processed_images/ec9d81e31e4e558e7c6b5f3c6f2a3cb3/3ac2b39ad528f8c8c5dc77c59abb683d.jpeg',
+  MAPLE_SYRUP: 'https://tb-static.uber.com/prod/image-proc/processed_images/53f3e89c9c6e0b7921913bc79e0dc7a1/3ac2b39ad528f8c8c5dc77c59abb683d.jpeg',
+  PANCAKE_MIX_2: 'https://tb-static.uber.com/prod/image-proc/processed_images/3c3d111e7d53fa962724747d0d16fe67/3ac2b39ad528f8c8c5dc77c59abb683d.jpeg',
+}; 
